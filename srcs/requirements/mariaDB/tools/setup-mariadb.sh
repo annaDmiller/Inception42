82c5ba656dc313ad5275b833fef764e5a95d9ee3
Ann Melnikova <amelniko@f6r13s2.clusters.42paris.fr>
2026-01-26 14:43:25 +0100

    Fix of the mariadb bash

--- srcs/requirements/mariaDB/tools/setup-mariadb.sh
+++ srcs/requirements/mariaDB/tools/setup-mariadb.sh
@@ -17,21 +17,23 @@ if [ ! -d "$DATADIR/mysql" ]; then
     echo "Start temp MariaDB..."
     mysqld --user=mysql --datadir="$DATADIR" --skip-networking &
     pid="$!"
-    sleep 5
+    sleep 10
 
     echo "Configure root, database and user..."
     mysql -u root <<-EOSQL
-        ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-        CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+        CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
         CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
         GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
         FLUSH PRIVILEGES;
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 EOSQL
 
     echo "Stop temp MariaDB..."
     kill "$pid"
     wait "$pid" 2>/dev/null || true
+else
+    echo "Data exists, starting normally..." 
 fi
 
 echo "Start MariaDB..."
-exec mysqld --user=mysql --datadir="$DATADIR"
+exec mysqld --user=mysql --datadir="$DATADIR" --skip-networking=0
