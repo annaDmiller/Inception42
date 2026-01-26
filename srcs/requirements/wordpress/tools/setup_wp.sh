644cc7335f935c614810ac62c54a5b7e58c87050
Ann Melnikova <amelniko@f6r13s2.clusters.42paris.fr>
2026-01-26 18:22:42 +0100

    Minor fix for final version

--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -20,7 +20,6 @@ if [ ! -f wp-config.php ]; then
 
     cp wp-config-sample.php wp-config.php
     
-    # Replace database settings using env vars
     sed -i "s/database_name_here/${MYSQL_DATABASE}/" wp-config.php
     sed -i "s/username_here/${MYSQL_USER}/" wp-config.php
     sed -i "s/password_here/${MYSQL_PASSWORD}/" wp-config.php
